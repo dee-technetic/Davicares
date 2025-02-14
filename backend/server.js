8df04a731bde759f028ba2e1763cd8b59ab679e9
adedoyin adesewa <adedolapoadedoyin541@gmail.com>
2025-02-14 21:22:33 +0100

    added a connection to frontend

--- backend/server.js
+++ backend/server.js
@@ -1,25 +1,27 @@
 import express from "express";
-import httpstatus from "http-status";
 import dotenv from "dotenv";
 import { dbConnect } from "./src/dbconnect/dbconfig.js";
 import appointmentRoute from "./src/routes/appointmentRoute.js";
 import adminRoute from "./src/routes/adminRoute.js";
 import bodyParser from "body-parser";
 import cors from "cors";
+import path from 'path';
+import { fileURLToPath } from 'url';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT;
 
+app.use(express.static(path.join(__dirname, 'frontend')));
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/api/admin',adminRoute);
 app.use('/api',appointmentRoute);
 
 app.get("/", (req, res) => {
-  res
-    .status(httpstatus.OK)
-    .json({ status: "200", message: "Welcome to Davicares API" });
+  res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
 });
 
 dbConnect().then(()=>{
